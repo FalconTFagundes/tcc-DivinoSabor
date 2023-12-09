0215dfe2e799ef0ddf5ebe82989e4b978600ffaf
FalconTFagundes <rafaelfagundes762@gmail.com>
2023-12-09 05:21:18 -0300

    resolvendo erro do diretório 'listar_evento_calendario.php

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -58,7 +58,9 @@ document.addEventListener('DOMContentLoaded', function () {
         // botão de mais links, número máximo de eventos em um determinado dia, se for true, o número de eventos será listado à altura da célula do dia
         dayMaxEvents: true,
 
-        events: './../pages/eventos/listar_evento_calendario.php'
+        events: 'listar_evento_calendario.php'
+
+
     });
 
     calendar.render();
