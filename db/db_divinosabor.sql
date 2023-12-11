43ca2f88422fb0ea924f660b7f4e0a1ac920c576
Rafael <rafaelfagundes762@gmail.com>
2023-12-10 23:18:38 -0300

    adicionando biblioteca Dompdf já funcionando para pedidos

--- db/db_divinosabor.sql
+++ db/db_divinosabor.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10/12/2023 às 22:42
+-- Tempo de geração: 11/12/2023 às 03:17
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -43,8 +43,9 @@ INSERT INTO `events` (`id`, `title`, `color`, `start`, `end`) VALUES
 (1, 'Teste Roxo', '#9E77F1', '2023-12-10 03:00:00', '2023-12-11 03:00:00'),
 (2, 'Teste Red', '#FF0831', '2023-12-15 03:00:00', '2023-12-16 03:00:00'),
 (3, 'Teste Blue', '#297BFF', '2023-12-19 03:00:00', '2023-12-20 03:00:00'),
-(4, 'teste Verde', '#00BD3f', '2023-12-07 03:00:00', '2023-12-08 03:00:00'),
-(5, 'teste amarelão', '#D4C200', '2023-12-22 03:00:00', '2023-12-23 03:00:00');
+(5, 'teste amarelão', '#D4C200', '2023-12-22 03:00:00', '2023-12-23 03:00:00'),
+(6, 'Teste', '#FF0831', '2023-12-26 23:00:00', '2023-12-27 03:00:00'),
+(7, 'Festa', '#00BD3f', '2023-12-07 03:00:00', '2023-12-08 03:00:00');
 
 -- --------------------------------------------------------
 
@@ -59,7 +60,7 @@ CREATE TABLE `pedidos` (
   `detalhes` varchar(70) NOT NULL,
   `cadastro` datetime NOT NULL,
   `alteracao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `ativo` char(1) NOT NULL DEFAULT 'A',
+  `ativo` char(1) NOT NULL DEFAULT 'D',
   `dataEntrega` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -68,7 +69,15 @@ CREATE TABLE `pedidos` (
 --
 
 INSERT INTO `pedidos` (`idpedidos`, `nome`, `pedido`, `detalhes`, `cadastro`, `alteracao`, `ativo`, `dataEntrega`) VALUES
-(18, 'test', 'eteste', 'teste', '2023-12-06 19:47:18', '2023-12-07 01:16:22', 'D', '2023-12-09');
+(1, 'Rafael', 'Festa de Aniverário', '200 salgados ao total', '2023-12-10 20:33:08', '2023-12-11 00:01:57', 'D', '2024-08-17'),
+(2, 'teste', 'teste', 'teste', '2023-12-10 22:24:10', '2023-12-11 01:24:43', 'D', '0111-11-11'),
+(3, 'Teste', 'teste', 'teste', '2023-12-10 22:50:03', '2023-12-11 01:50:03', 'D', '2025-11-11'),
+(4, 'teste', 'tagasg', 'gasgag', '2023-12-10 22:50:15', '2023-12-11 01:50:15', 'D', '2024-11-11'),
+(5, 'teste', 'teste', 'teste', '2023-12-10 22:50:30', '2023-12-11 01:50:30', 'D', '2023-12-11'),
+(6, 'test', 't', 't', '2023-12-10 22:50:38', '2023-12-11 01:50:38', 'D', '1111-11-11'),
+(7, 'test', 't', 't', '2023-12-10 22:50:45', '2023-12-11 01:50:45', 'D', '1111-11-11'),
+(8, 't', 't', 't', '2023-12-10 22:50:55', '2023-12-11 01:50:55', 'D', '0000-00-00'),
+(9, 'test', 't', 't', '2023-12-10 22:51:01', '2023-12-11 01:51:01', 'D', '1111-11-11');
 
 -- --------------------------------------------------------
 
@@ -125,13 +134,13 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `idpedidos` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `idpedidos` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
--- pages/pedidos/listarPedidos.php
+++ pages/pedidos/listarPedidos.php
@@ -12,167 +12,168 @@ include_once './func/dashboard.php';
 <button type="button" class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#modalCadPedido">
   <i class="fa-solid fa-plus"></i> Cadastrar Pedido
 </button>
-<br><br>
+<a href="gerarRelatPedido.php"><button type="button" class="btn btn-outline-secondary"> <i class="fa-solid fa-print"></i>  Gerar Relatório</button> </a>
+  <br><br>
 
 
 
-<div style="height: 400px;">
-  <table class="table-financeira table table-hover">
-    <thead>
-      <tr>
-        <th scope="col" width="5%">Código</th>
-        <th scope="col" width="25%">Nome</th>
-        <th scope="col" width="15%">Pedido</th>
-        <th scope="col" width="30%">Detalhes</th>
-        <th scope="col" width="30%">Data de Entrega</th>
-        <th scope="col" width="25%">Ações</th>
-      </tr>
-    </thead>
-    <tbody>
+  <div style="height: 400px;">
+    <table class="table-financeira table table-hover">
+      <thead>
+        <tr>
+          <th scope="col" width="5%">Código</th>
+          <th scope="col" width="25%">Nome</th>
+          <th scope="col" width="15%">Pedido</th>
+          <th scope="col" width="30%">Detalhes</th>
+          <th scope="col" width="30%">Data de Entrega</th>
+          <th scope="col" width="25%">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
 
-      <?php
-      $dataAtual = date("Y-m-d");  // Formato ISO 8601!!!!!!
-      $dataSeteDiasAntes = date("Y-m-d", strtotime("-7 days"));
+        <?php
+        $dataAtual = date("Y-m-d");  // Formato ISO 8601!!!!!!
+        $dataSeteDiasAntes = date("Y-m-d", strtotime("-7 days"));
 
 
-      $retornoListarPedidos = listarGeral('idpedidos, nome, pedido, detalhes, cadastro, alteracao, ativo, dataEntrega', 'pedidos');
-      if (is_array($retornoListarPedidos) && !empty($retornoListarPedidos)) {
-        foreach ($retornoListarPedidos as $itemPedido) {
-          $idPedido = $itemPedido->idpedidos;
-          $nomePedido = $itemPedido->nome;
-          $pedido = $itemPedido->pedido;
-          $detalhesPedido = $itemPedido->detalhes;
-          $ativoPedido = $itemPedido->ativo;
-          $dataEntrega = $itemPedido->dataEntrega;
+        $retornoListarPedidos = listarGeral('idpedidos, nome, pedido, detalhes, cadastro, alteracao, ativo, dataEntrega', 'pedidos');
+        if (is_array($retornoListarPedidos) && !empty($retornoListarPedidos)) {
+          foreach ($retornoListarPedidos as $itemPedido) {
+            $idPedido = $itemPedido->idpedidos;
+            $nomePedido = $itemPedido->nome;
+            $pedido = $itemPedido->pedido;
+            $detalhesPedido = $itemPedido->detalhes;
+            $ativoPedido = $itemPedido->ativo;
+            $dataEntrega = $itemPedido->dataEntrega;
 
-          $dataEntregaFormat = date("d/m/Y", strtotime($dataEntrega)); //passando para o formato br
+            $dataEntregaFormat = date("d/m/Y", strtotime($dataEntrega)); //passando para o formato br
 
-          $classeData = '';
-          if (strtotime($dataAtual) >= strtotime($dataEntrega)) {
-            $classeData = 'entregaVermelha';
-          } elseif (strtotime($dataAtual) >= strtotime('-7 days', strtotime($dataEntrega)) && strtotime($dataAtual) < strtotime($dataEntrega)) {
-            $classeData = 'entregaAmarela';
-          } else {
-            $classeData = 'entregaVerde';
+            $classeData = '';
+            if (strtotime($dataAtual) >= strtotime($dataEntrega)) {
+              $classeData = 'entregaVermelha';
+            } elseif (strtotime($dataAtual) >= strtotime('-7 days', strtotime($dataEntrega)) && strtotime($dataAtual) < strtotime($dataEntrega)) {
+              $classeData = 'entregaAmarela';
+            } else {
+              $classeData = 'entregaVerde';
+            }
+
+
+
+
+        ?>
+
+            <tr>
+              <th scope="row"><?php echo $idPedido; ?></th>
+              <td><?php echo $nomePedido; ?></td>
+              <td><?php echo $pedido; ?></td>
+              <td><?php echo $detalhesPedido; ?></td>
+              <td class="<?php echo $classeData; ?>"><?php echo $dataEntregaFormat; ?></td>
+              <td>
+                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                  <?php
+                  if ($ativoPedido == 'A') {
+                  ?>
+                    <button type='button' class='btn btn-outline-dark' onclick="ativarGeral(<?php echo $idPedido; ?>,'desativar','ativarPedidos','listarPedidos', 'Pedido marcado como concluído');"> <i class="fa-solid fa-unlock"></i> Não Concluído</button>
+                  <?php
+                  } else {
+                  ?>
+                    <button type='button' class='btn btn-outline-success' onclick="ativarGeral(<?php echo $idPedido; ?>, 'ativar', 'ativarPedidos','listarPedidos', 'Pedido marcado como não concluído');"><i class="fa-solid fa-lock"></i> Concluído</button>
+
+                  <?php
+                  }
+                  ?>
+                  <button type="submit" class="btn btn-outline-danger" onclick="excGeral('<?php echo $idPedido; ?>', 'excluirPedidos', 'listarPedidos', 'Certeza que deseja excluir?', 'Operação Irreversível!')"><i class="fa-solid fa-trash"></i> Excluir</button>
+                </div>
+              </td>
+            </tr>
+        <?php
           }
-
-
-
-
-      ?>
-
-          <tr>
-            <th scope="row"><?php echo $idPedido; ?></th>
-            <td><?php echo $nomePedido; ?></td>
-            <td><?php echo $pedido; ?></td>
-            <td><?php echo $detalhesPedido; ?></td>
-            <td class="<?php echo $classeData; ?>"><?php echo $dataEntregaFormat; ?></td>
-            <td>
-              <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                <?php
-                if ($ativoPedido == 'A') {
-                ?>
-                  <button type='button' class='btn btn-outline-dark' onclick="ativarGeral(<?php echo $idPedido; ?>,'desativar','ativarPedidos','listarPedidos', 'Pedido marcado como concluído');"> <i class="fa-solid fa-unlock"></i> Não Concluído</button>
-                <?php
-                } else {
-                ?>
-                  <button type='button' class='btn btn-outline-success' onclick="ativarGeral(<?php echo $idPedido; ?>, 'ativar', 'ativarPedidos','listarPedidos', 'Pedido marcado como não concluído');"><i class="fa-solid fa-lock"></i> Concluído</button>
-
-                <?php
-                }
-                ?>
-                <button type="submit" class="btn btn-outline-danger" onclick="excGeral('<?php echo $idPedido; ?>', 'excluirPedidos', 'listarPedidos', 'Certeza que deseja excluir?', 'Operação Irreversível!')"><i class="fa-solid fa-trash"></i> Excluir</button>
-              </div>
-            </td>
-          </tr>
-      <?php
+        } else {
+          echo "<div class='alert alert-warning' style='text-align: center;' role='alert'>";
+          echo "Nenhum Registro Encontrado";
+          echo "</div>";
         }
-      } else {
-        echo "<div class='alert alert-warning' style='text-align: center;' role='alert'>";
-        echo "Nenhum Registro Encontrado";
-        echo "</div>";
-      }
-      ?>
+        ?>
 
-    </tbody>
-  </table>
-</div>
+      </tbody>
+    </table>
+  </div>
 
 
-<style>
-  .entregaVermelha,
-  .entregaAmarela,
-  .entregaVerde {
-    border-radius: 10px;
-    padding: 15px 30px;
-    color: #fff;
-    font-weight: bold;
-    text-align: center;
-    display: inline-block;
-    margin: 10px;
-    cursor: pointer;
-    transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
-    position: relative;
-    z-index: 0;
-  }
+  <style>
+    .entregaVermelha,
+    .entregaAmarela,
+    .entregaVerde {
+      border-radius: 10px;
+      padding: 15px 30px;
+      color: #fff;
+      font-weight: bold;
+      text-align: center;
+      display: inline-block;
+      margin: 10px;
+      cursor: pointer;
+      transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+      position: relative;
+      z-index: 0;
+    }
 
-  .entregaVermelha {
-    background: linear-gradient(45deg, #FF6347, #FF4580);
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  }
+    .entregaVermelha {
+      background: linear-gradient(45deg, #FF6347, #FF4580);
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
 
-  .entregaAmarela {
-    background: linear-gradient(45deg, #FFD700, #FFA500);
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  }
+    .entregaAmarela {
+      background: linear-gradient(45deg, #FFD700, #FFA500);
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
 
-  .entregaVerde {
-    background: linear-gradient(45deg, #32CD32, #008000);
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  }
+    .entregaVerde {
+      background: linear-gradient(45deg, #32CD32, #008000);
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
 
-  /* Hover effect */
-  .entregaVermelha:hover,
-  .entregaAmarela:hover,
-  .entregaVerde:hover {
-    transform: scale(1.05);
-    box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
-  }
-</style>
+    /* Hover effect */
+    .entregaVermelha:hover,
+    .entregaAmarela:hover,
+    .entregaVerde:hover {
+      transform: scale(1.05);
+      box-shadow: 0 8px 12px rgba(0, 0, 0, 0.2);
+    }
+  </style>
 
 
 
 
-<div class="modal fade" id="modalCadPedido" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header" style="background-color:blueviolet; color: white;">
-        <h5 class="modal-title" id="exampleModalLabel">Cadastrar Pedido</h5>
+  <div class="modal fade" id="modalCadPedido" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header" style="background-color:blueviolet; color: white;">
+          <h5 class="modal-title" id="exampleModalLabel">Cadastrar Pedido</h5>
+        </div>
+        <form name="frmCadPedido" method="POST" id="frmCadPedido" class="frmCadPedido" action="#">
+          <div class="modal-body">
+            <div class="mb-3">
+              <label for="nomePedido" class="form-label">Nome do Cliente</label>
+              <input type="text" class="form-control" name="nomePedido" id="nomePedido" aria-describedby="nomePedido" required>
+            </div>
+            <div class="mb-3">
+              <label for="pedido" class="form-label">Pedido</label>
+              <input type="text" class="form-control" name="pedido" id="pedido" required>
+            </div>
+            <div class="mb-3">
+              <label for="detalhesPedido" class="form-label">Detalhes</label>
+              <textarea class="form-control" name="detalhesPedido" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <div class="mb-3">
+              <label for="dataEntregaPedido" class="form-label">Data de Entrega</label>
+              <input type="date" class="form-control" name="dataEntregaPedido" id="dataEntregaPedido" required>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fechar</button>
+            <button type="submit" class="btn btn-primary" onclick="cadGeral('frmCadPedido','modalCadPedido','cadastrarPedidos','listarPedidos');">Cadastrar</button>
+          </div>
+        </form>
       </div>
-      <form name="frmCadPedido" method="POST" id="frmCadPedido" class="frmCadPedido" action="#">
-        <div class="modal-body">
-          <div class="mb-3">
-            <label for="nomePedido" class="form-label">Nome do Cliente</label>
-            <input type="text" class="form-control" name="nomePedido" id="nomePedido" aria-describedby="nomePedido" required>
-          </div>
-          <div class="mb-3">
-            <label for="pedido" class="form-label">Pedido</label>
-            <input type="text" class="form-control" name="pedido" id="pedido" required>
-          </div>
-          <div class="mb-3">
-            <label for="detalhesPedido" class="form-label">Detalhes</label>
-            <textarea class="form-control" name="detalhesPedido" id="exampleFormControlTextarea1" rows="3"></textarea>
-          </div>
-          <div class="mb-3">
-            <label for="dataEntregaPedido" class="form-label">Data de Entrega</label>
-            <input type="date" class="form-control" name="dataEntregaPedido" id="dataEntregaPedido" required>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Fechar</button>
-          <button type="submit" class="btn btn-primary" onclick="cadGeral('frmCadPedido','modalCadPedido','cadastrarPedidos','listarPedidos');">Cadastrar</button>
-        </div>
-      </form>
     </div>
   </div>
-</div>
