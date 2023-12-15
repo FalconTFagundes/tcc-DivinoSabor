fe0c2768e7916e5ba1153c6c1a908bb7d9d04fe4
Geisa-Martins-Pereira <geisaprogramacaocontrol@gmail.com>
2023-12-14 22:24:29 -0300

    atualização financeiro

--- pages/financeiro/listarFinanceiro.php
+++ pages/financeiro/listarFinanceiro.php
@@ -1,47 +1,77 @@
     <div class="cardBox">
         <div class="card">
-        <div>
-            <div class="numbers">1.502</div>
-            <div class="cardName">Clientes</div>
-        </div>
-        </div>
-        <div class="iconBox">
-            <box-icon name='user-circle'></box-icon>
+            <div>
+                <div class="numbers">1.502</div>
+                <div class="cardName">Clientes</div>
+            </div>
+
+            <div class="iconBox">
+                <i class="fa-regular fa-user"></i>
+            </div>
         </div>
 
 
         <div class="card">
-        <div>
-            <div class="numbers">584</div>
-            <div class="cardName">Vendas mensais</div>
-        </div>
-        
-        <div class="iconBox">
-            <box-icon name='user-circle'></box-icon>
-        </div>
+            <div>
+                <div class="numbers">584</div>
+                <div class="cardName">Vendas mensais</div>
+            </div>
 
+            <div class="iconBox">
+                <i class="fa-regular fa-money-bill-1"></i>
+            </div>
+        </div>
 
         <div class="card">
-        <div>
-            <div class="numbers">63</div>
-            <div class="cardName">Défict</div>
-        </div>
-        </div>
-        <div class="iconBox">
-            <box-icon name='user-circle'></box-icon>
+            <div>
+                <div class="numbers">63</div>
+                <div class="cardName">Défict</div>
+            </div>
+
+            <div class="iconBox">
+                <i class='bx bx-line-chart-down'></i>
+            </div>
         </div>
 
-        <div>
-
-        
-        
         <div class="card">
-        <div>
-            <div class="numbers">704</div>
-            <div class="cardName">Lucro</div>
+            <div>
+                <div class="numbers">704</div>
+                <div class="cardName">Lucro</div>
+            </div>
+
+            <div class="iconBox">
+                <i class="fa-solid fa-chart-line"></i>
+            </div>
         </div>
+    </div>
+
+
+
+    <!-- GRÁFICOS -->
+
+    <div class="details">
+        <div class="vendasRecentes">
+            <div class="cardHeader">
+                <h2>Vendas recentes</h2>
+                <a href="" class="btn">Ver tudo</a>
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th scope="col" width="5%">Código do pedido</th>
+                        <th scope="col" width="25%">Nome do cliente</th>
+                        <th scope="col" width="25%">Pacote escolhido</th>
+                        <th scope="col" width="25%">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>1</tr>
+                    <tr>João</tr>
+                    <tr>Diamante</tr>
+                    <tr><span class="status concluido">Concluído</span></tr>
+                </tbody>
+            </table>
+
         </div>
-        <div class="iconBox">
-            <box-icon name='user-circle'></box-icon>
-        </div>
-    </div> 
+    </div>
